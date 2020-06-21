8fcf62e97f5e43eb87b0598540eacc19e0bdd2d8
Lanc33llis <scdelance@gmail.com>
2020-06-21 18:50:58 -0500

    current edititon

--- 3WayPro.cpp
+++ 3WayPro.cpp
@@ -7,6 +7,7 @@
 #include <iostream>
 #include <future>
 #include <chrono>
+#include <algorithm>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
@@ -15,6 +16,57 @@ const double PI = 3.14159265;
 
 using namespace std;
 using namespace cv;
+
+bool CASort(vector<Point> lhs, vector<Point> rhs) { return (contourArea(lhs, true) > contourArea(rhs, true)); }
+
+bool LTRSort(Point lhs, Point rhs) { return lhs.x < rhs.x; }
+
+inline double distance(Point pt0, Point pt1)
+{
+	return sqrt(pow(pt1.x - pt0.x, 2) + pow(pt1.y - pt0.y, 2));
+}
+
+inline double distance(double x1, double y1, double x2, double y2)
+{
+	return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+}
+
+bool disSort(Point lhs, Point rhs) { return (distance(Point(0, 0), lhs) < distance(Point(0, 0), rhs)); }
+
+vector<Point> generalizeHull(vector<Point> hull)
+{
+	vector<Point> g;
+
+	for (size_t t = 0; t < hull.size(); t++)
+	{
+		double d;
+
+		auto& p1 = hull[t];
+		for (size_t a = 0; a < hull.size(); a++)
+		{
+			auto& p2 = hull[a];
+			d = distance(p1, p2);
+
+			if (a == t)
+				continue;
+
+			if (d > 10)
+			{
+				continue;
+			}
+
+			else
+			{
+				hull.erase(hull.begin() + a);
+				t = 0;
+				break;
+			}
+		}
+
+	}
+
+	return hull;
+}
 
 //returns the angle measure of the middle point as a double and degree
 double dAngle(Point pt0, Point pt1, Point pt2)
@@ -27,15 +79,11 @@ double dAngle(Point pt0, Point pt1, Point pt2)
 	return C;
 }
 
-inline double distance(Point pt0, Point pt1)
-{
-	return sqrt(pow(pt1.x - pt0.x, 2) + pow(pt1.y - pt0.y, 2));
-}
 
 Mat aTT(Mat mat)
 {
 
-	adaptiveThreshold(mat, mat, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 85, 11);
+	adaptiveThreshold(mat, mat, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 79, 0 );
 
 	return mat;
 }
@@ -43,6 +91,7 @@ Mat aTT(Mat mat)
 Mat analyzeHands(Mat& mat, bool& temp)
 {
 	medianBlur(mat, mat, 1);
+	//cvtColor(mat, mat, COLOR_BGR2HSV);	
 	Mat combined;
 
 	Mat bgr[3];
@@ -98,8 +147,8 @@ int main()
 
 			if (waitKey(1) == 122)
 			{
-				gotHands = true;
-				getHands = false;
+				gotHands = !gotHands;
+				getHands = !getHands;
 			}
 		}
 
@@ -144,19 +193,26 @@ int main()
 		}
 
 		drawContours(img, hand, -1, Scalar(128, 0, 128), 2, LINE_AA, noArray(), 214783647, Point(xmid * .6, ymid * .3));
-		if (!hulls.empty())
+		sort(hulls.begin(), hulls.end(), CASort);
+		if (!hulls.empty() && hulls[0].size() > 6)
 		{
-			drawContours(img, hulls, -1, Scalar(0, 128, 128), 2, LINE_AA, noArray(), 214783647, Point(xmid * .6, ymid * .3));
-			auto thumbangle = dAngle(hulls[0][6], hulls[0][0], hulls[0][1]);
+			drawContours(img, hulls, 0, Scalar(50, 50, 128), 2, LINE_AA, noArray(), 214783647, Point(xmid * .6, ymid * .3));
+			auto pinkangle = dAngle(hulls[0][7], hulls[0][0], hulls[0][1]);
 			auto pointangle = dAngle(hulls[0][0], hulls[0][1], hulls[0][2]);
-			auto middleangle = dAngle(hulls[1][6], hulls[2][0], hulls[3][1]);
-			auto ringangle = dAngle(hulls[2][6], hulls[3][0], hulls[4][1]);
-			auto thumbangle = dAngle(hulls[3][6], hulls[4][0], hulls[5][1]);
+			auto middleangle = dAngle(hulls[0][1], hulls[0][2], hulls[0][3]);
+			auto ringangle = dAngle(hulls[0][2], hulls[0][3], hulls[0][4]);
+			auto thumbangle = dAngle(hulls[0][3], hulls[0][4], hulls[0][5]);
 
 			double angles[5] = { thumbangle, pointangle, middleangle, ringangle, thumbangle };
 
-			for (size_t i = 0; i < hulls[0].size(); i++)
-			{
+			vector<vector<Point>> fingerpoints(1);
+
+			auto g = generalizeHull(hulls[0]);
+			sort(g.begin(), g.end(), LTRSort);
+			size_t j = g.size() - 5;
+
+			for (size_t i = g.size() - 1; i > j; i--)
+			{ /*
 				double x, y, s;
 				double ra;
 
@@ -166,7 +222,7 @@ int main()
 				case 0:
 					ra = 360 - angles[i];;
 					break;
-				case 1: 
+				case 1:
 					ra = 360 - angles[i];
 					break;
 				case 2:
@@ -176,7 +232,7 @@ int main()
 					}
 					else
 					{
-						ra = 90 - tan(atan2(p2.x, -p2.y);
+						ra = 90 - tan(atan2(p2.x, -p2.y));
 					}
 					break;
 				case 5:
@@ -191,8 +247,41 @@ int main()
 				x = p2.x; y = y3;
 
 				line(img, p1, Point(x, y), Scalar(0, 100, 100), 2, LINE_AA);
+				*/
+
+
+				auto& x1 = g[i].x;
+				auto& y1 = g[i].y;
+				auto& x2 = g[i - 1].x;
+				auto& y2 = g[i - 1].y;
+				auto& x3 = g[i - 2].x;
+				auto& y3 = g[i - 2].y;
+
+				double m1, m2;
+
+				if (x2 - x1 == 0 || x3 - x2 == 0)
+				{
+					continue;
+				}
+
+				m1 = (y2 - y1) / (x2 - x1);
+				m2 = (y3 - y2) / (x3 - x2);
+
+				double b1, b2;
+				b1 = y1 - x1 * m1;
+				b2 = y1 - x1 * m1;
+
+				double bt = sqrt(pow(m1, 2) + 1);
+				double nm1 = m1 * bt, nc = b1 * bt;
+				double ny = (nm1 * x3 + nc);
+
+				fingerpoints[0].push_back(g[i - 1]);
+				fingerpoints[0].push_back(Point(x3, ny));
+
+
 			}
 			
+			drawContours(img, fingerpoints, -1, Scalar(200, 10, 10), 2, LINE_AA, noArray(), 214783647, Point(xmid * .6, ymid * .3));
 
 		}
 		
